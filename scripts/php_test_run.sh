b8d6db2af7481ea89d884ea9736d4b89b2092ed3
Issam Kh <issam.kharkhach@gmail.com>
2020-02-07 19:44:49 +0000

    fixes phpunit issues

--- scripts/php_test_run.sh
+++ scripts/php_test_run.sh
@@ -1,4 +1,4 @@
 php bin/console cache:clear --env=test
 php bin/console doctrine:database:drop --force --env=test
 php bin/console doctrine:database:create --env=test
-php bin/phpunit
+php vendor/phpunit/phpunit/phpunit
